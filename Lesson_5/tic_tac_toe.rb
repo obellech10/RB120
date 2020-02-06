364730eb3525a7ffdd7b2b7b72b6ca48a0ea6430
Oscar Bellechasse <obellech10@gmail.com>
2020-02-06 07:54:59 -0700

    Update display_board method, add new method to clear and display board.

--- Lesson_5/tic_tac_toe.rb
+++ Lesson_5/tic_tac_toe.rb
@@ -1,10 +1,9 @@
 require 'pry'
-system 'clear'
 
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-    [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-    [[1, 5, 9], [3, 5, 7]]
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                  [[1, 5, 9], [3, 5, 7]]
 
   def initialize
     @squares = {}
@@ -51,15 +50,15 @@ class Board
   end
 
   def reset
-    (1..9).each {|key| @squares[key] = Square.new }
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 end
 
 class Square
-  INITIAL_MARKER = " "
+  INITIAL_MARKER = ' '.freeze
   attr_accessor :marker
 
-  def initialize(marker=INITIAL_MARKER)
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
 
@@ -81,8 +80,8 @@ class Player
 end
 
 class TTTGame
-  HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
+  HUMAN_MARKER = 'X'.freeze
+  COMPUTER_MARKER = 'O'.freeze
   attr_reader :board, :human, :computer
 
   def initialize
@@ -92,17 +91,16 @@ class TTTGame
   end
 
   def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ""
+    puts 'Welcome to Tic Tac Toe!'
+    puts ''
   end
 
   def display_goodbye_message
-    clear_screen
-    puts "Thanks for playing Tic Tac Toe. Goodbye!"
+    clear
+    puts 'Thanks for playing Tic Tac Toe. Goodbye!'
   end
 
-  def display_board(clear=true)
-    clear_screen if clear
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -117,6 +115,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -135,7 +138,7 @@ class TTTGame
   end
 
   def display_result
-    clear_screen
+    clear
     display_board
     if board.detect_winner == HUMAN_MARKER
       puts "You won!"
@@ -159,10 +162,11 @@ class TTTGame
   end
 
   def play
+    clear
     display_welcome_message
 
     loop do
-      display_board(false)
+      display_board
 
       loop do
         human_moves
@@ -171,13 +175,13 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
 
       board.reset
-      clear_screen
+      clear
       puts "Let's play again!"
     end
     display_goodbye_message
@@ -185,7 +189,7 @@ class TTTGame
 
   private
 
-  def clear_screen
+  def clear
     system('clear') || system('clr')
   end
 end
